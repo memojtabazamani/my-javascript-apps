cf658da9fe36b4ef9a7d512d5f481e70ddd2003f
memojtabazamani <memojtabazamani@gmail.com>
2021-08-15 14:58:46 +0430

    انیمیشن پایه طراحی شد

--- loadingchars/app.js
+++ loadingchars/app.js
@@ -1,1 +1,17 @@
 console.log("That Work!");
+
+$(function() {
+
+    const parentElm = $("#loading");
+
+    const childs = parentElm.find('span');
+    console.log(childs);
+
+    let delayAnimation = 0;
+    childs.map((child) => {
+        const elm = childs[child];
+        $(elm).css({"animation-name": "loadingAnimation", "animation-delay": delayAnimation + "s"});
+        delayAnimation+= 0.3;
+    })
+
+})
